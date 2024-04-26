d332b497ed6db4d731566c6f939353e663b24d5a
yulia <dementevau21@email.com>
2024-04-26 12:44:05 +0300

    QA79.2

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,23 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        {
-            int initialaccount = 500; //начальный счет
 
-            int replenishment = 1000; //пополнение
+        int initialAccount = 200; //начальный счет
 
-            int balance = initialaccount + replenishment; //общий баланс счета
-            System.out.println("Итоговый баланс:" + balance);
+        int replenishment = 1200; //пополнение
+        int balance = initialAccount + replenishment; //общий баланс счета
+        System.out.println("Итоговый баланс:" + balance);
 
-            int bonus = balance * 1 / 100;
-            if (balance > 1000) {
-                balance = 1000;
-            }
+        int bonus; // бонус
+        if (balance > 1000) {
+            bonus = replenishment / 100;
             System.out.println("Итоговый бонус:" + bonus);
+        } else {
+
+            bonus = 0;
+
+            System.out.println("Итоговый бонус:" + bonus);
+
         }
     }
 }
